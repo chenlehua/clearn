67367c6876d5c4e254fe841d9053d81d4e5d950c
gavinlhchen <13692238295@163.com>
2023-03-05 19:29:15 +0800

    feat(clearn) add shell sort

--- shell-sort/shell_sort.c
+++ shell-sort/shell_sort.c
@@ -1,10 +1,27 @@
-//
-// Created by gavinlhchen on 3/5/23.
-//
-
 #include <stdio.h>
 
-int main(){
-    printf("sfsf");
+int *sortArray(int *nums, int numsSize, int *returnSize) {
+    *returnSize = numsSize;
+    for (int gap = numsSize / 2; gap > 0; gap /= 2) {
+        for (int i = gap; i < numsSize; i++) {
+            int tmp = nums[i], j;
+            for (j = i - gap; j >= 0 && nums[j] > tmp; j -= gap) {
+                nums[j + gap] = nums[j];
+            }
+            nums[j + gap] = tmp;
+        }
+    }
+    return nums;
+}
+
+int main() {
+    int nums[] = {49, 38, 65, 97, 76, 13, 27, 49, 55, 04};
+    int returnSize;
+    int *result = sortArray(nums, 10, &returnSize);
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", result[i]);
+    }
+    printf("\nreturnSize: %d", returnSize);
     return 0;
 }
+
